b4f9c040f4811b720353ee141c90b6aaa102a095
paramgit <paramjit.profs@gmail.com>
2022-05-05 16:14:22 +0530

    paytm commit

--- wallet-service/src/test/java/capgemini/paramjit/walletservice/WalletServiceApplicationTests.java
+++ wallet-service/src/test/java/capgemini/paramjit/walletservice/WalletServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package capgemini.paramjit.walletservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
