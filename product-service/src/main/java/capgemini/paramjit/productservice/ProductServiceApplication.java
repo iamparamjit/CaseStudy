dae605521318ea9dc2471b5870f5a8e3e1628d18
paramgit <paramjit.profs@gmail.com>
2022-05-12 17:22:20 +0530

    final commit

--- product-service/src/main/java/capgemini/paramjit/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/capgemini/paramjit/productservice/ProductServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -17,6 +19,7 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+		
 	}
-
+	
 }
